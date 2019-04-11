e2845124889db22b29b22841608d12a74016df01
hm9817 <49473098+hm9817@users.noreply.github.com>
2019-04-11 13:33:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,8 @@
-/*var wei = document.getElementById("weixin");
-var div1 = document.getElementById('div1');
-var weix = document.getElementById('weix');
-var one = document.getElementsByClassName('one');
-wei.onmousemove = function() {
-	one[0].style.display = 'block';
-    div1.style.display = 'block';
-}
-wei.onmouseout=function(){
-	one[0].style.display = 'none';
-    div1.style.display = 'none';
-}
-*/
-var wei = document.getElementById("weixin");
-wei.onmousemove = function(a1) {
-	a1.style.backgroundImage="url(img/fo_15.png)";
-    div1.style.display = 'block';
-}
-wei.onmouseout=function(){
-	a1.style.display = 'none';
-    div1.style.display = 'none';
-}
+$(document).ready(function() {
+	$(".header_a").mouseover(function(){
+		$(this).children("#div1").css("display","block");
+	}).mouseout(function(){
+		$(this).children("#div1").css("display","none");
+	})
+})
 
